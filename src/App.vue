70128a9464d58f57cb41c2fe30ebe5be4323fb42
innacp <inna.chapkevych@gmail.com>
2024-07-04 11:28:15 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+
+
+<template>
+  <section class="add-task-container">
+    <h2>Add Task</h2>
+    <form class="todo-form">
+      <label>
+        <input class="new-todo" type="text" name="todo">
+      </label>
+      <button class="add-button">+</button>
+    </form>
+
+  </section>
+
+</template>
+
+<script setup>
+  
+</script>
+
+<style scoped>
+</style>
